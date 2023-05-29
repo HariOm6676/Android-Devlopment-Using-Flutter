85ba7712d8dfba7184d51b514b818e2b47dacc81
HariOm6676 <118880027+HariOm6676@users.noreply.github.com>
2023-05-30 00:39:12 +0530

    Day 2 Completed

---
+++ lib/home_page.dart
@@ -0,0 +1,22 @@
+import "package:flutter/material.dart";
+
+// ignore: camel_case_types
+class home_page extends StatelessWidget {
+  final int part = 1;
+  final String name = "Hari Om Shukla";
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Catalogue App"),
+      ),
+      body: Center(
+        child: Container(
+          child: Text("Welcome To $name Repositories part $part"),
+        ),
+      ),
+      drawer: Drawer(),
+    );
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,17 @@
 import 'package:flutter/material.dart';
-void main()
-{
+import 'package:test_app/home_page.dart';
+
+void main() {
   runApp(const MyApp());
 }
+
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home:Material(
-        child: Center(
-          child: Text("Welcome To Hari Om Shukla's Repositories"),
-        )
-      )
-    ); 
+    return MaterialApp(
+      home: home_page(),
+    );
   }
 }
